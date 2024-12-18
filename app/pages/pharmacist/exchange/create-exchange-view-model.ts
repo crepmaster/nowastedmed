072ff5f4f4ccd2840a12ec54407d9cc815abdcda
crepmaster <50635535+crepmaster@users.noreply.github.com>
2024-12-18 20:20:46 +0100

    Update app/pages/pharmacist/exchange/create-exchange-view-model.ts [BoltSync]

--- app/pages/pharmacist/exchange/create-exchange-view-model.ts
+++ app/pages/pharmacist/exchange/create-exchange-view-model.ts
@@ -1,7 +1,6 @@
 import { Observable, ImageSource } from '@nativescript/core';
 import { Medicine } from '../../../models/medicine.model';
 import { MedicineService } from '../../../services/medicine.service';
-import * as qrcode from 'qrcode';
 
 export class CreateExchangeViewModel extends Observable {
     private medicineService: MedicineService;
@@ -9,6 +8,7 @@ export class CreateExchangeViewModel extends Observable {
     public qrCode: string = '';
     public qrCodeImage: ImageSource | null = null;
     public errorMessage: string = '';
+    public isGenerating: boolean = false;
 
     constructor(medicine: Medicine) {
         super();
@@ -18,18 +18,25 @@ export class CreateExchangeViewModel extends Observable {
 
     async onGenerateQR() {
         try {
+            this.set('isGenerating', true);
+            this.set('errorMessage', '');
+
             const exchange = await this.medicineService.createExchange(this.medicine.id);
-            this.qrCode = await this.medicineService.generateQRCode(exchange);
+            this.qrCode = exchange.qrCode;
             
-            // Generate QR code image
-            const qrImageData = await qrcode.toDataURL(this.qrCode);
-            this.qrCodeImage = await ImageSource.fromBase64(qrImageData.split(',')[1]);
+            // Convert data URL to ImageSource
+            if (this.qrCode.startsWith('data:image/png;base64,')) {
+                const base64Data = this.qrCode.replace('data:image/png;base64,', '');
+                this.qrCodeImage = await ImageSource.fromBase64(base64Data);
+                this.notifyPropertyChange('qrCodeImage', this.qrCodeImage);
+            }
             
             this.notifyPropertyChange('qrCode', this.qrCode);
-            this.notifyPropertyChange('qrCodeImage', this.qrCodeImage);
         } catch (error) {
             console.error('Error generating QR code:', error);
             this.set('errorMessage', 'Failed to generate QR code');
+        } finally {
+            this.set('isGenerating', false);
         }
     }
 }
