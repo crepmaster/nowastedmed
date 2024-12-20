e5255b854d3140b0b8f884c8204d9a88e5dd16fa
crepmaster <50635535+crepmaster@users.noreply.github.com>
2024-12-20 22:16:43 +0100

    Update app/pages/login/login-page.ts [BoltSync]

--- app/pages/login/login-page.ts
+++ app/pages/login/login-page.ts
@@ -2,7 +2,6 @@ import { NavigatedData, Page } from '@nativescript/core';
 import { LoginViewModel } from './login-view-model';
 
 export function onNavigatingTo(args: NavigatedData) {
-    console.log('Login page navigating to...');
     const page = <Page>args.object;
     if (!page.bindingContext) {
         page.bindingContext = new LoginViewModel();
