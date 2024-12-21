fda074caaf4c44fd3ac5d29cd4f4c0ec8d750adb
crepmaster <50635535+crepmaster@users.noreply.github.com>
2024-12-21 15:31:19 +0100

    Update app/services/crud/pharmacy.crud.service.ts [BoltSync]

--- app/services/crud/pharmacy.crud.service.ts
+++ app/services/crud/pharmacy.crud.service.ts
@@ -1,14 +1,17 @@
 import { Observable } from '@nativescript/core';
 import { Pharmacist } from '../../models/user.model';
 import { DatabaseService } from '../database/database.service';
+import { AuthService } from '../../services/auth.service';
 
 export class PharmacyCrudService extends Observable {
     private static instance: PharmacyCrudService;
     private databaseService: DatabaseService;
+    private authService: AuthService;
 
     private constructor() {
         super();
         this.databaseService = DatabaseService.getInstance();
+        this.authService = AuthService.getInstance();
     }
 
     static getInstance(): PharmacyCrudService {
@@ -16,6 +19,23 @@ export class PharmacyCrudService extends Observable {
             PharmacyCrudService.instance = new PharmacyCrudService();
         }
         return PharmacyCrudService.instance;
+    }
+
+    async isPharmacyNameTaken(pharmacyName: string, excludeId?: string): Promise<boolean> {
+        const registeredUsers = this.authService.getRegisteredUsers();
+        return registeredUsers.some(user => 
+            user.role === 'pharmacist' && 
+            user.pharmacyName?.toLowerCase() === pharmacyName.toLowerCase() &&
+            user.id !== excludeId
+        );
+    }
+
+    async isEmailTaken(email: string, excludeId?: string): Promise<boolean> {
+        const registeredUsers = this.authService.getRegisteredUsers();
+        return registeredUsers.some(user => 
+            user.email.toLowerCase() === email.toLowerCase() &&
+            user.id !== excludeId
+        );
     }
 
     async getAll(): Promise<Pharmacist[]> {
@@ -27,10 +47,26 @@ export class PharmacyCrudService extends Observable {
     }
 
     async create(pharmacy: Partial<Pharmacist>): Promise<Pharmacist> {
+        // Check for uniqueness
+        if (await this.isPharmacyNameTaken(pharmacy.pharmacyName!)) {
+            throw new Error('A pharmacy with this name already exists');
+        }
+        if (await this.isEmailTaken(pharmacy.email!)) {
+            throw new Error('This email is already registered');
+        }
+
         return this.databaseService.createPharmacy(pharmacy);
     }
 
     async update(id: string, data: Partial<Pharmacist>): Promise<Pharmacist> {
+        // Check for uniqueness, excluding the current pharmacy
+        if (data.pharmacyName && await this.isPharmacyNameTaken(data.pharmacyName, id)) {
+            throw new Error('A pharmacy with this name already exists');
+        }
+        if (data.email && await this.isEmailTaken(data.email, id)) {
+            throw new Error('This email is already registered');
+        }
+
         return this.databaseService.updatePharmacy(id, data);
     }
 
