f3c7ba76f1a69c18948db45c21ed5cbd0c751553
crepmaster <50635535+crepmaster@users.noreply.github.com>
2024-12-28 21:10:29 +0100

    Update app/app.ts [BoltSync]

--- app/app.ts
+++ app/app.ts
@@ -3,11 +3,19 @@ import { AuthService } from './services/auth.service';
 import { AdminService } from './services/admin.service';
 import { PermissionsService } from './services/permissions.service';
 import { NavigationService } from './services/navigation.service';
+import { DemoDataService } from './services/demo/demo-data.service';
+import { DebugUtil } from './utils/debug.util';
 
 // Initialize services
 AuthService.getInstance();
 AdminService.getInstance();
 PermissionsService.getInstance();
 NavigationService.getInstance();
+
+// Initialize demo data
+DemoDataService.getInstance().initializeDemoData();
+
+// Debug: Print registered users
+DebugUtil.printRegisteredUsers();
 
 Application.run({ moduleName: 'app-root' });
